36d03bd3d16b9542a1e7a89e1049fa7e7d12c53f
Mafooch <joshuamaffuccio@gmail.com>
2016-02-06 16:05:41 -0500

    Update controller and at to_zip helper methods

--- app/helpers/zips_helper.rb
+++ app/helpers/zips_helper.rb
@@ -1,2 +1,9 @@
 module ZipsHelper
+  def to_zip value
+    # change value to a Zip if not already a Zip
+    # unlike with AR we may never create a Zip instance everytime we have a zip
+    # document. this lets us make the instance which gives other object oriented
+    # functionality
+    value.is_a?(Zip) ? value : Zip.new(value)
+  end
 end
